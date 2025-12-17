5aaab4aa50b2497e95a867c74e8351cb1508a60d
leventev <levente.vamos@protonmail.com>
2025-12-17 17:28:16 +0100

    remove build artifacts

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/elzett/projects/aoc2025/day1/target/debug/day1: /home/elzett/projects/aoc2025/day1/src/main.rs
--- day1/target/debug/deps/day1-cec9b34865ab49a7.d
+++
@@ -1,9 +0,0 @@
-/home/elzett/projects/aoc2025/day1/target/debug/deps/day1-cec9b34865ab49a7.d: src/main.rs Cargo.toml
-
-/home/elzett/projects/aoc2025/day1/target/debug/deps/libday1-cec9b34865ab49a7.rmeta: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
